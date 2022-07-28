e3694ffb7c124dd451ed470a7527762cdd9075ad
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-07-28 13:53:58 +0200

    q&e

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+
+const questions = document.querySelectorAll('.question');
+questions.forEach(question => {
+  //console.log(question);
+  const btn = question.querySelector('.question-btn');
+  //console.log(btn);
+  btn.addEventListener('click', function(){
+
+    questions.forEach(item => {
+      if(item !== question){
+        item.classList.remove('show-text')
+      }
+    });
+    
+    question.classList.toggle('show-text');
+  });
+});
+
+
+
+
+//const btns = document.querySelectorAll('.question-btn');
+// btns.forEach(btn => {
+//   btn.addEventListener('click', function(e){
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   })
+// });
+
+
